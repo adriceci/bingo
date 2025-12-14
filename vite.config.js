81ddf729305c628fa3f372b3bfd87f9e52e61d9f
Adrian Cecilia <adriancecilia@fisioreact.com>
2025-12-14 19:07:10 +0100

    fix(vite): revert server port to 5173 and disable HMR configuration

--- vite.config.js
+++ vite.config.js
@@ -19,11 +19,8 @@ export default defineConfig({
     ],
     server: {
         host: '0.0.0.0',
-        port: 5193,
-        hmr: {
-            host: 'localhost',
-            port: 5193,
-        },
+        port: 5173,
+        hmr: false,
         cors: true,
     },
 });
