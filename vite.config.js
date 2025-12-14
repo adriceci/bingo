451e75fcac8b7dc977d9e7b69887f8835bc10c76
Adrian Cecilia <adriancecilia@fisioreact.com>
2025-12-14 19:25:13 +0100

    fix(vite): update input configuration and HMR settings in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,30 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.js',
             refresh: true,
         }),
-        tailwindcss(),
-        vue(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
     server: {
+        https: true,
         host: '0.0.0.0',
         port: 5173,
         hmr: {
-            host: 'bingo.adriancecilia.com',
-            port: 443,
-            protocol: 'wss',
+            host: 'localhost',
+            port: 5193,
         },
+        cors: true,
     },
 });
